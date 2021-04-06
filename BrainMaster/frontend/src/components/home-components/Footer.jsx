7e4654be2e5506bb60230a7307d71ed83405d4f4
oshadeegangangana <oshadeegangangana@gmail.com>
2021-04-06 14:01:57 +0530

    code review

--- BrainMaster/frontend/src/components/home-components/Footer.jsx
+++ BrainMaster/frontend/src/components/home-components/Footer.jsx
@@ -7,7 +7,7 @@ class Footer extends Component {
                 <div className="footer-area">
                     <p>
                         © Copyright 2021 <a href="#">Brain Master App</a>. All right
-                        reserved. Developed by <a>GG Programming</a>.
+                        reserved. Developed by <a>Beginners Group 03</a>.
                         {/*Change Accordingly*/}
                     </p>
                 </div>
